6ea81d69fe932a54b2ff7b9ab772b6a87d644ac1
Kshitiz Agrawal <kshitizagrawal@outlook.com>
2022-01-18 21:15:53 +0530

    remove web vitals

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM, {render} from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Users from './Users';
 import Images from './Images';
@@ -18,8 +17,3 @@ render(
   ,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
